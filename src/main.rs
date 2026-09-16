a975ee56af79a0d75e179355e3211fb2f93abe57
Ansh Chawla <anshchawla262007@gmail.com>
2026-09-16 10:23:15 -0400

    Change motto on branch

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 const CREW_NAME: &str = "the unnamed crew";
 
 /// Your crew's motto. You will both change this one too, earlier and separately.
-const MOTTO: &str = "Ship it and see";
+const MOTTO: &str = "Measure twice push once";
 
 fn main() {
     println!("=== {} ===", CREW_NAME);
